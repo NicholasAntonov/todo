a601f147846102993b51e8b43a844b3f8704b57b
Nicholas Antonov <nicholasantonov@gmail.com>
2014-05-20 16:13:16 -0400

    First Commit

---
+++ learning/Scripts/_references.js
@@ -0,0 +1,2 @@
+﻿/// <reference path="../js/learning.js" />
+/// <reference path="~/Scripts/_scalejs.intellisense.js" />
---
+++ learning/app/todo/viewmodels/todoViewModel.js
@@ -0,0 +1,105 @@
+﻿/*global define, localStorage */
+define([
+    'sandbox!todo',
+    'app/todo/viewmodels/itemViewModel'
+], function (
+    sandbox,
+    itemViewModel
+) {
+    'use strict';
+
+    return function () {
+        var observable = sandbox.mvvm.observable,
+            observableArray = sandbox.mvvm.observableArray,
+            has = sandbox.object.has,
+            computed = sandbox.mvvm.computed,
+            raise = sandbox.state.raise,
+            //properties
+            items = observableArray(),
+            newItem = observable(),
+            checkAll,
+            completedItems,
+            viewableOptions,
+            currentView = observable(),
+            viewableItems;
+        
+        viewableItems = computed(function () {
+            var view = currentView();
+
+            if (view === 'Active') {
+                return items().where('!$.completed()').toArray();
+            }
+            if (view === 'Completed') {
+                return items().where('$.completed()').toArray();
+            }
+            return items();
+        });
+
+        function toItem(itemVM) {
+            return {
+                title: itemVM.title(),
+                completed:itemVM.completed()
+            };
+        }
+
+        function toItemViewModel(item) {
+            return itemViewModel(item, items);
+        }
+
+        function addItem() {
+            var item = newItem();
+            if (has(item, "trim") && item.trim()) {
+                items.push( itemViewModel( { title: item, completed: false } ) );
+            }
+            newItem("");
+        }
+
+        checkAll = computed({
+            read: function () {
+                return items().all("$.completed()");
+            },
+            write: function (value) {
+                items().forEach(function (item) { item.completed(value); });
+            }
+        });
+
+        completedItems = computed(function () {
+            return items().where("$.completed()").toArray();
+        });
+
+        function removeCompletedItems() {
+            completedItems().forEach(function (item) {
+                item.remove();
+            });
+        }
+
+        viewableOptions = ['All', 'Active', 'Completed'].map(function (name) {
+            return {
+                name: name,
+                raiseEvent: function () {
+                    raise('todo.' + name);
+                }
+            };
+        });
+
+        computed(function () {
+            localStorage['todos-scalejs'] = JSON.stringify(items().map(toItem));
+        });
+
+        if (has(localStorage['todos-scalejs'])) {
+            items(JSON.parse(localStorage['todos-scalejs']).map(toItemViewModel));
+        }
+
+        return {
+            items: items,
+            newItem: newItem,
+            addItem: addItem,
+            checkAll: checkAll,
+            completedItems: completedItems,
+            removeCompletedItems: removeCompletedItems,
+            viewableOptions: viewableOptions,
+            currentView: currentView,
+            viewableItems: viewableItems
+        };
+    };
+});
